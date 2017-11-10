3f8529409ad123b5c1f703960e937695f76c7a04
Stacy DeRuiter <stacy@calvin.edu>
2017-11-10 15:19:15 -0500

    create para_boot and update pred_plot to work with geeglm() output

---
+++ R/para_boot.R
@@ -0,0 +1,68 @@
+#' carry out parametric bootstrap to get CIs for model predictions
+#'
+#' @param model a fitted model object created by lm() or glm() or geeglm()
+#' @param new_data data frame with data for which to make predictions and generate bootstrap CIs
+#' @param predictor the covariate for which to make predictions. other predictors in the model will be held constant at their median value, or the most commonly observed value in the dataset.
+#' @param nboot number of bootstrap iterations. Defaults to 1000. Ignored if boot is FALSE.
+#' @param conf_level confidence level as a proportion, default is 0.95 for 95 percent confidence
+#'
+#' @export
+#' @return a data.frame with variables CIlower and CIupper, the lower and upper bounds of the parametric bootstrap CI
+
+para_boot <- function(model, new_data, predictor, nboot, conf_level=0.95){
+
+#get the "design(ish) matrix"
+coefs <- coef(model)
+xmats <- list()
+f <- as.character(formula(model))
+f <- paste("~", f[3], sep="")
+xmat <- model.matrix(as.formula(f), new_data)
+
+
+#parametric bootstrap to get CIs on the mean factor level effects
+#Assume (this is reasonable given our model) that the
+#parameter estimates
+#follow a multivariate normal distribution with means
+#equal to the parameter estimates from the fitted model and
+#variance-covariance matrix also from the fitted model
+#(accessed via summary(model)$cov.scaled)
+# Use rmvnorm (from mvnorm library) to sample coefs from this
+#MVN (say maybe 5000 iterations) and multiply by the design matrix
+#to get predictions (make sure the 2 matrices are the right shapes
+#for matrix multiplication... ).  values should be a matrix with
+#nrows=number of parameters (factor combination levels) and
+#ncol=number of iterations). get the mvn values that will be the
+#bootstrap "output" for each iteration
+
+boot_coefs <- t(mvtnorm::rmvnorm(nboot, mean=model$coef,
+                        sigma=summary(model)$cov.scaled,
+                        method="svd"))
+boot_CI <- list()
+#now multiply the prediction-data
+#by the coefficients  (with the appropriate link function! to get
+#predicted value in each condition for each #iteration:
+if (model$family$link == "logit"){
+    boot_pred <- mosaic::ilogit(xmat %*% boot_coefs)
+}
+if (model$family$link == "identity"){
+    boot_pred <- xmat %*% boot_coefs
+}
+if (model$family$link == "log"){
+    boot_pred <- exp(xmat %*% boot_coefs)
+}
+if (model$family$link == 'probit'){
+  boot_pred <- VGAM::probit(xmat %*% boot_coefs, inverse=TRUE)
+}
+if (model$family$link == 'cloglog'){
+  boot_pred <- VGAM::cloglog(xmat %*% boot_coefs, inverse=TRUE)
+}
+if (model$family$link == 'inverse'){
+  boot_pred <- 1/(xmat %*% boot_coefs)
+}
+# then to turn that into a CI, find percentile based CIs for
+#each row (each row is one combo of factor levels).
+boot_CI <- data.frame(upper=apply(boot_pred, 1,
+                                  quantile, probs= 1 - ((1-conf_level)/2)))
+boot_CI$lower <- apply(boot_pred, 1, quantile, probs= (1-conf_level)/2 )
+return(boot_CI)
+}
--- R/pred_plot.R
+++ R/pred_plot.R
@@ -1,10 +1,13 @@
 #' make prediction plots for lm and glm objects using ggformula.
 #'
-#' @param model a fitted model object created by lm() or glm()
+#' @param model a fitted model object created by lm() or glm() or geeglm()
 #' @param predictor the covariate for which to make predictions. other predictors in the model will be held constant at their median value, or the most commonly observed value in the dataset.
 #' @param xlab X axis label for plot (defaults to name of predictor variable)
 #' @param ylab Y axis label for plot (defaults to "Predictions from Fitted Model")
-#' @param conf_int logical: should 95 percent confidence intervals be shown (as error bars or confidence band)?
+#' @param conf_int logical: should confidence intervals be shown (as error bars or confidence band)?
+#' @param conf_level confidence level as a proportion, default is 0.95 for 95 percent confidence
+#' @param boot logical: should CIs be derived via a parametric bootstrap? Defaults to FALSE except if the model is a GEE, then defaults to TRUE.
+#' @param nboot number of bootstrap iterations. Defaults to 1000. Ignored if boot is FALSE.
 #' @param new_data_out logical: should data set used for predictions be output? If TRUE, result is a list including the plot object and the data frame.
 #' @param ... Additional arguments to be passed to plotting function
 #'
@@ -13,7 +16,12 @@
 
 
 pred_plot <- function(model, predictor, xlab=NULL, ylab=NULL,
-                      conf_int=TRUE, ...){
+                      conf_level=0.95, conf_int=TRUE,
+                      boot=FALSE, nboot=1000, new_data_out=FALSE,
+                      ...){
+  if ('gee' %in% class(model)){
+    boot=TRUE
+  }
   fixed_vals <- get_fixed(model)
   data <- model$model
   #make dataset for predictions
@@ -26,13 +34,28 @@ pred_plot <- function(model, predictor, xlab=NULL, ylab=NULL,
   names(new_data)[2:ncol(fixed_vals)] <- names(fixed_vals)[-xi]
   }
   names(new_data)[1] <- predictor
-  #make predictions
-  pred <- predict(model, newdata=new_data, type='response', se.fit=TRUE)
-  new_data$preds <- pred$fit
-  if (conf_int){
-  new_data$CIl <- pred$fit - 1.96*pred$se.fit
-  new_data$CIu <- pred$fit + 1.96*pred$se.fit
-  }
+  if (!boot){
+    #make predictions
+    if (conf_int){
+      pred <- predict(model, newdata=new_data, type='response', se.fit=TRUE)
+      new_data$preds <- pred$fit
+      zstar <- qnorm((1-conf_level)/2, lower.tail=FALSE)
+      new_data$CIl <- pred$fit - zstar*pred$se.fit
+      new_data$CIu <- pred$fit + zstar*pred$se.fit
+    }else{#no CIs
+      pred <- predict(model, newdata=new_data, type='response')
+      new_data$preds <- pred
+      }
+  }else{#bootstrap CIs
+    pred <- predict(model, newdata=new_data, type='response')
+    new_data$preds <- pred
+    if (conf_int){
+      boot_CI <- para_boot(model, new_data, predictor,
+                           nboot, conf_level)
+      new_data$CIl <- boot_CI$lower
+      new_data$CIu <- boot_CI$upper
+    }
+    }
 
   if (is.null(xlab)){
     xlab = predictor
@@ -42,24 +65,30 @@ pred_plot <- function(model, predictor, xlab=NULL, ylab=NULL,
   }
   form <- as.formula(paste("preds ~ ", predictor))
   form2 <- as.formula(paste("CIl + CIu ~ ", predictor))
+
   #make plot if categorical predictor
   if (conf_int){
-  gf_point(form, data=new_data) %>%
-    gf_labs(x=xlab, y=ylab) %>%
-    gf_errorbar(form2, data=new_data)
+  P <- ggformula::gf_point(form, data=new_data) %>%
+    ggformula::gf_labs(x=xlab, y=ylab) %>%
+    ggformula::gf_errorbar(form2, data=new_data)
   }else{
-    gf_point(form, data=new_data) %>%
-      gf_labs(x=xlab, y=ylab)
+  P <- ggformula::gf_point(form, data=new_data) %>%
+    ggformula::gf_labs(x=xlab, y=ylab)
   }
   #make plot if quant predictor
   if (conf_int){
-  gf_line(form, data=new_data) %>%
-    gf_labs(x=xlab, y = ylab) %>%
-    gf_ribbon(form2, data=new_data)
+  P <- ggformula::gf_line(form, data=new_data) %>%
+    ggformula::gf_labs(x=xlab, y = ylab) %>%
+    ggformula::gf_ribbon(form2, data=new_data)
   }else{
-    gf_line(form, data=new_data) %>%
-      gf_labs(x=xlab, y = ylab)
+   P <- ggformula::gf_line(form, data=new_data) %>%
+     ggformula::gf_labs(x=xlab, y = ylab)
   }
-
+  P
+  if (new_data_out){
+    return(list(plot=P, new_data=new_data))
+  }else{
+    return(P)
+  }
 }
 
